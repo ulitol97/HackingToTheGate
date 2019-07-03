e640edce5c394caae2d237b84c8fd8fed0349877
ulitol97 <UO251436@uniovi.es>
2019-07-03 19:51:12 +0200

    Enemy walking realistic.

--- Assets/Scripts/Game/Enemies/LogEnemy.cs
+++ Assets/Scripts/Game/Enemies/LogEnemy.cs
@@ -35,14 +35,26 @@ namespace Game.Enemies
         public float attackRadius;
 
         /// <summary>
+        /// Unity Animator component in charge of animating the enemy sprite to simulate actions.
+        /// </summary>
+        /// <remarks>The animator attributes are cached below for quicker access</remarks>
+        private Animator _enemyAnimator;
+        private static readonly int AnimatorWakeUp = Animator.StringToHash("wakeUp");
+        private static readonly int AnimatorMoveX = Animator.StringToHash("moveX");
+        private static readonly int AnimatorMoveY = Animator.StringToHash("moveY");
+
+        /// <summary>
         /// Function called when the Enemy script is loaded into the game.
         /// Sets up the enemy's current state, target and references to the Unity components modified on runtime.
         /// </summary>
         private void Start()
         {
             currentState = EnemyState.Idle;
-            target = GameObject.FindWithTag("Player").transform;
             _enemyRigidBody = GetComponent<Rigidbody2D>();
+            _enemyAnimator = GetComponent<Animator>();
+            
+            target = GameObject.FindWithTag("Player").transform;
+            
         }
 
         /// <summary>
@@ -65,21 +77,44 @@ namespace Game.Enemies
         /// and updates the enemy's position in case the target is in the chase radius but not in the
         /// attack radius and the enemy is not staggered.
         /// </summary>
+        /// <remarks>It also handles the animations in case the enemy wakes up or
+        /// goes to sleep.</remarks>
         private void CheckDistance()
         {
             float distanceToTarget = Vector3.Distance(transform.position, target.position);
             
             // Approach but never more than attack radius.
-            if (distanceToTarget <= chaseRadius && distanceToTarget > attackRadius
-                && (currentState == EnemyState.Idle || currentState == EnemyState.Walk) 
-                && currentState != EnemyState.Staggered)
+            if (distanceToTarget <= chaseRadius && distanceToTarget > attackRadius)
             {
-                Vector3 movement = Vector3.MoveTowards(transform.position, 
-                    target.position, moveSpeed * Time.deltaTime);
+                if ((currentState == EnemyState.Idle || currentState == EnemyState.Walk) 
+                    && currentState != EnemyState.Staggered)
+                {
+                    Vector3 movement = Vector3.MoveTowards(transform.position, 
+                        target.position, moveSpeed * Time.deltaTime);
                 
-                _enemyRigidBody.MovePosition(movement);
-                ChangeState(EnemyState.Walk);
+                
+                    ChangeAnim(movement - transform.position);
+                    _enemyRigidBody.MovePosition(movement);
+                
+                    // Walk and wake up animation if needed.
+                    ChangeState(EnemyState.Walk);
+                    _enemyAnimator.SetBool(AnimatorWakeUp, true);
+                }
             }
+            else if (distanceToTarget > attackRadius)
+            {
+                _enemyAnimator.SetBool(AnimatorWakeUp, false);
+            }
+        }
+        /// <summary>
+        /// Function managing the animation the enemy should make regarding it's moving direction.
+        /// </summary>
+        /// <param name="direction"></param>
+        private void ChangeAnim(Vector2 direction)
+        {
+            direction = direction.normalized;
+            _enemyAnimator.SetFloat(AnimatorMoveX, direction.x);
+            _enemyAnimator.SetFloat(AnimatorMoveY, direction.y);
         }
 
     }
