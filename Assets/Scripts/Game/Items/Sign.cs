1dc85a2849986d15a57a8f90cb47ba058f4cf29a
ulitol97 <UO251436@uniovi.es>
2019-07-03 02:01:36 +0200

    Documented sign script.

--- Assets/Scripts/Game/Items/Sign.cs
+++ Assets/Scripts/Game/Items/Sign.cs
@@ -4,40 +4,62 @@ using UnityEngine.UI;
 
 namespace Game.Items
 {
+    /// <summary>
+    /// The Sign class represents an in-game Sign the player can approach and read.
+    /// It holds some text to be shown to the player when interacted with.
+    /// </summary>
     public class Sign : MonoBehaviour
     {
+        /// <summary>
+        /// UI element containing the dialog template image.
+        /// </summary>
         public GameObject dialogBox;
+        
+        /// <summary>
+        /// UI element in charge of rendering the dialog text.
+        /// </summary>
         public Text dialogText;
 
+        /// <summary>
+        /// String identifying the dialog the sign must show the player.
+        /// </summary>
         public string dialogKey;
 
+        /// <summary>
+        /// Boolean flag marked true if the player is in range to interact with the sign.
+        /// </summary>
         private bool _playerInRange;
-        
-        void Start()
-        {
-        
-        }
 
-        // Update is called once per frame
-        void Update()
+        /// <summary>
+        /// Function called on each frame the Sign the script is attached to is present into the game.
+        /// Checks for user controller input and if the player is close enough to interact with the sign contents.
+        /// </summary>
+        private void Update()
         {
-            // Redefinir un axis.
             if (Input.GetKeyDown(KeyCode.Joystick1Button0) && _playerInRange)
             {
-                Debug.Log(dialogBox.activeInHierarchy);
-                if (dialogBox.activeInHierarchy)
-                    dialogBox.SetActive(false);
-                else
-                {
-                    dialogText.text = Globals.Instance.TipsTable[dialogKey];
-                    dialogBox.SetActive(true);
-                }
-                
+                ToggleSignText();
             }
         }
 
         /// <summary>
-        /// 
+        /// If the sign's text is being displayed, it is hidden.
+        /// If the sign's text is no hidden
+        /// </summary>
+        private void ToggleSignText()
+        {
+            if (dialogBox.activeInHierarchy)
+                dialogBox.SetActive(false);
+            else
+            {
+                dialogText.text = Globals.Instance.TipsTable[dialogKey];
+                dialogBox.SetActive(true);
+            }
+        }
+
+        /// <summary>
+        /// Checks for collision events between the sign and other objects with collision capabilities.
+        /// If the object colliding with the sign is the player, marks that the player is in range.
         /// </summary>
         /// <param name="other">Collider object that initiated contact.</param>
         /// <exception cref="NotImplementedException"></exception>
@@ -51,7 +73,8 @@ namespace Game.Items
         }
 
         /// <summary>
-        /// 
+        /// Checks for end of collision events between the sign and other with objects collision capabilities.
+        /// If the object that stopped colliding with the sign is the player, marks that the player is out of range.
         /// </summary>
         /// <param name="other">Collider object that finished contact.</param>
         private void OnTriggerExit2D(Collider2D other)
