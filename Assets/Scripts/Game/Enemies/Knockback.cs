985dfc090ecd55814d9ffe90abdb6c9dc3b0c721
ulitol97 <UO251436@uniovi.es>
2019-07-03 17:24:29 +0200

    Knockback added to player agains enemies.

---
+++ Assets/Scripts/Game/Enemies/Knockback.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Game.Enemies
+{
+    /// <summary>
+    /// The Knockback class use's Unity's RigidBody components to apply knockback forces
+    /// between interacting objects.
+    /// </summary>
+    public class Knockback : MonoBehaviour
+    {
+
+        /// <summary>
+        /// A multiplying factor to the knockback force applied.
+        /// </summary>
+        public float thrust;
+
+        /// <summary>
+        /// Amount of time the knock back is meant to last.
+        /// </summary>
+        public float knockTime;
+
+        /// <summary>
+        /// Checks for start-collision events affecting the game element attached to this scrip and
+        /// triggers the corresponding logic of the object that collided with the attack.
+        /// If the enemy entered the collision, it is knocked back proportionally to the distance to the player.
+        /// </summary>
+        /// <param name="other">Collider object that initiated contact.</param>
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Enemy"))
+            {
+                Rigidbody2D otherBody = other.GetComponent<Rigidbody2D>();
+                if (otherBody != null)
+                {
+                    // Allow applying forces
+                    otherBody.isKinematic = false;
+                    Vector2 distance = otherBody.transform.position - transform.position;
+                    // Apply multiplying factor
+                    distance = distance.normalized * thrust;
+                    
+                    otherBody.AddForce(distance, ForceMode2D.Impulse);
+                    
+                    // Start knockback disable
+                    StartCoroutine(EndKnock(otherBody));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Co routine in charge of stopping the knockback effect on the knocked back game elements
+        /// after a certain time has passed by (<see cref="knockTime"/>).
+        /// </summary>
+        /// <param name="other">Body of the knocked back element to be managed.</param>
+        /// <returns></returns>
+        private IEnumerator EndKnock(Rigidbody2D other)
+        {
+            if (other != null)
+            {
+                yield return new  WaitForSeconds(knockTime);
+                StopKnockBack(other);
+            }
+        }
+
+        /// <summary>
+        /// Resets a game element's body speed and disables its capability to be moved.
+        /// </summary>
+        /// <param name="other">Body of the knocked back element to be stopped.</param>
+        private void StopKnockBack(Rigidbody2D other)
+        {
+            other.velocity = Vector2.zero;
+            other.isKinematic = true;
+        }
+    }
+}
--- Assets/Scripts/Game/Enemies/LogEnemy.cs
+++ Assets/Scripts/Game/Enemies/LogEnemy.cs
@@ -2,33 +2,63 @@
 
 namespace Game.Enemies
 {
+    /// <summary>
+    /// The LogEnemy class is in charge of handling all the logic related to Log enemies
+    /// found in game.
+    /// </summary>
     public class LogEnemy : Enemy
     {
+        /// <summary>
+        /// Transform object containing the coordinates the enemy should follow.
+        /// </summary>
         public Transform target;
+        
+        /// <summary>
+        /// Transform object containing the coordinates the enemy shall patrol.
+        /// </summary>
         public Transform homePosition;
 
+        /// <summary>
+        /// Radius of the terrain area the enemy shall patrol.
+        /// </summary>
         public float chaseRadius;
 
+        /// <summary>
+        /// Minimum distance needed for the enemy to attack. A player won't approach the player
+        /// further than its attack radius.
+        /// </summary>
         public float attackRadius;
 
-        // Start is called before the first frame update
+        /// <summary>
+        /// Function called when the Enemy script is loaded into the game.
+        /// Sets up the enemy's current state, target and references to the Unity components modified on runtime.
+        /// </summary>
         private void Start()
         {
             target = GameObject.FindWithTag("Player").transform;
         }
 
-        // Update is called once per frame
+        /// <summary>
+        /// Function called on each frame the Enemy script is present into the game.
+        /// Checks for the target's position.
+        /// </summary>
         private void Update()
         {
             CheckDistance();
         }
 
+        /// <summary>
+        /// Computes the distance between the enemy and its target (<see cref="target"/>)
+        /// and updates the enemy's position in case the target is in the chase radius but not in the
+        /// attack radius.
+        /// </summary>
         private void CheckDistance()
         {
             float distanceToTarget = Vector3.Distance(transform.position, target.position);
+            
+            // Approach but never more than attack radius.
             if (distanceToTarget <= chaseRadius && distanceToTarget > attackRadius)
             {
-                // Approach but never more than attack radius.
                 transform.position = Vector3.MoveTowards(transform.position, 
                     target.position, moveSpeed * Time.deltaTime);
             }
