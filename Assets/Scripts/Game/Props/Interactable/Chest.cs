a2d7c2efe766007da929ef196fee10d5c5ff970f
ulitol97 <UO251436@uniovi.es>
2019-07-04 12:50:05 +0200

    Created chest object inheriting interactable.

---
+++ Assets/Scripts/Game/Props/Interactable/Chest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Game.Props.Interactable
+{
+    /// <summary>
+    /// The Chest class inherits from Interactable.
+    /// Represents an in-game treasure chest.
+    /// </summary>
+    public class Chest : Interactable
+    {
+        
+    }
+}
